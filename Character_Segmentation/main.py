a5ae371a36e346b0d7b4df3ee43b3e82e358190c
Dishank Bansal <dishankbansal09@gmail.com>
2016-10-18 23:53:11 +0530

    Tuning the thresholding parameters

--- Character_Segmentation/main.py
+++ Character_Segmentation/main.py
@@ -1,24 +1,41 @@
 import cv2 
 import numpy as np
+import sys
+import time
 
 #-------------Thresholding Image--------------#
 
-src_img = cv2.imread('./data/img_1.jpg', 0)
-# blur = cv2.GaussianBlur(src_img,(3,3),0)
-gud_img = cv2.adaptiveThreshold(src_img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,73,2)
-ret3,thr_img = cv2.threshold(gud_img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-# gud_img = cv2.fastNlMeansDenoising(gud_img, None,20,7,21)
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-closing = cv2.morphologyEx(thr_img, cv2.MORPH_CLOSE, kernel)
+src_img = cv2.imread('./data/img_2.jpg', 1)
+grey_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
 
-# ret,thr_img = cv2.threshold(src_img,127,255,cv2.THRESH_BINARY)
+gud_img = cv2.adaptiveThreshold(grey_img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,101,2)
+# ret3,thr_img = cv2.threshold(gud_img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+closing = cv2.morphologyEx(gud_img, cv2.MORPH_CLOSE, kernel, iterations = 2) # To remove "pepper-noise"
+
+kernel1 = np.array([[1,0,1],[0,1,0],[1,0,1]], dtype = np.uint8)
+final = cv2.erode(closing,kernel1,iterations = 1)
+
+# blur = cv2.bilateralFilter(erosion,9,75,75)
 
 
 #-------------Displaying Image----------------#
 
 cv2.namedWindow('Source Image', cv2.WINDOW_NORMAL)
 cv2.namedWindow('Threshold Image', cv2.WINDOW_NORMAL)
+
 cv2.imshow("Source Image", src_img)
-cv2.imshow("Threshold Image", closing)
-cv2.waitKey(0)
+cv2.imshow("Threshold Image", final)
+
+
+#-------------Closing Windows-----------------#
+
+k = cv2.waitKey(0)
+if k & 0xFF == ord('s'):
+	comment = input("Comment:-\n ")
+	cv2.imwrite('./data/test_result/'+comment+'.jpg',final)
+	# cv2.imwrite('OKAY.jpg',final)
+	print("Completed")
+else:
+	cv2.destroyAllWindows()
